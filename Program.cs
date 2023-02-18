59598678c9d8eecde256c5a6f94a863f1efe6959
Yaroslaw-aw <manumission@mail.ru>
2023-02-18 21:23:57 +0300

    19 task ready steady go

--- Program.cs
+++ Program.cs
@@ -5,19 +5,50 @@
 //23432 -> да
 
 Console.WriteLine("Введите пятизначное число");
-string is_a_number_a_palindrome = Console.ReadLine();
 
-if (is_a_number_a_palindrome.Length != 5)
+int is_a_number_a_palindrome;
+
+int.TryParse(Console.ReadLine(), out is_a_number_a_palindrome);
+
+bool palindrome(int number)
 {
-    Console.WriteLine("Введено неверное число");
+    if (number < 0 || number % 10 == 0)
+    {
+        return false; 
+    }
+    else if (number == 0)
+    {
+        return true;
+    }
+    else
+    {
+        int revers_number = 0;
+        int temp_number = number;
+        int last_number;        
+
+        while (number > revers_number)
+        {
+            last_number = number % 10;
+
+            temp_number = number;
+
+            number /= 10;
+
+            revers_number = revers_number * 10 + last_number;
+        }
+        if (temp_number == revers_number || number == revers_number)
+        {
+            return true;
+        }
+        return false;
+    }
 }
-else if (is_a_number_a_palindrome[0] == is_a_number_a_palindrome[4] && is_a_number_a_palindrome[1] == is_a_number_a_palindrome[3])
+
+if (palindrome(is_a_number_a_palindrome))
 {
-    Console.WriteLine("Число является палиндромом");
-}
+    Console.WriteLine("Число палиндром");
+}    
 else
 {
     Console.WriteLine("Число не является палиндромом");
 }
-
-// проверку на то, что введены именно циферные значения вводить не стал
